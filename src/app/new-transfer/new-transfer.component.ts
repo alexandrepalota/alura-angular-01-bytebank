06299f0021ab572b54c4df3a5de91f598a38eae0
Alexandre Palota da Silva <alexandre.palota@gmail.com>
2022-05-07 20:54:55 -0300

    Creating form event

--- src/app/new-transfer/new-transfer.component.ts
+++ src/app/new-transfer/new-transfer.component.ts
@@ -1,10 +1,12 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
 
 @Component({
-    selector: 'app-new-transfer',
-    templateUrl: './new-transfer.component.html',
-    styleUrls: ['./new-transfer.component.scss']
+  selector: 'app-new-transfer',
+  templateUrl: './new-transfer.component.html',
+  styleUrls: ['./new-transfer.component.scss'],
 })
 export class NewTransferComponent {
-
+  transfer() {
+    console.log('Requested new transfer');
+  }
 }
