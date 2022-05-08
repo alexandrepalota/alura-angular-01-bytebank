6c3bc3a73a95d679f329f5d3152e4cb502b5c0b3
Alexandre Palota da Silva <alexandre.palota@gmail.com>
2022-05-07 21:50:57 -0300

    Send data to component

--- src/app/statement/statement.component.ts
+++ src/app/statement/statement.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-statement',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./statement.component.scss']
 })
 export class StatementComponent implements OnInit {
+
+  @Input() transfer: any;
 
   constructor() { }
 
