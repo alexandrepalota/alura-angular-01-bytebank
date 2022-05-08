914e199248c95b92e3d051f4353a6182fecfda12
Alexandre Palota da Silva <alexandre.palota@gmail.com>
2022-05-07 23:32:26 -0300

    Router module

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NewTransferComponent } from './new-transfer/new-transfer.component';
+import { StatementComponent } from './statement/statement.component';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+  {path: '', redirectTo: 'statement', pathMatch: 'full'},
+  {path: 'statement', component: StatementComponent},
+  {path: 'new-transfer', component: NewTransferComponent}
+]
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
