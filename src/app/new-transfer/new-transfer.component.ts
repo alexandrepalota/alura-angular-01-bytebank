1b7224aed2b34379c05327d7ef62c00912f98967
Alexandre Palota da Silva <alexandre.palota@gmail.com>
2022-05-07 21:14:56 -0300

    Export data with Output

--- src/app/new-transfer/new-transfer.component.ts
+++ src/app/new-transfer/new-transfer.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
-
+import { Component, Output, EventEmitter } from '@angular/core';
 @Component({
   selector: 'app-new-transfer',
   templateUrl: './new-transfer.component.html',
   styleUrls: ['./new-transfer.component.scss'],
 })
 export class NewTransferComponent {
+  @Output() onTransfer = new EventEmitter<any>();
+
+  value: number = 12;
+  target: number = 222;
+
   transfer() {
     console.log('Requested new transfer');
+    const valueEmit = { value: this.value, target: this.target };
+    this.onTransfer.emit(valueEmit);
   }
 }
